add12d3ee85612f3358ec18488c386f809a76159
ome1ia <o_melnikova@inbox.ru>
2018-03-31 18:14:31 +0400

    Add files via upload
    Французская Полинезия

---
+++ french_polynisia.php
@@ -0,0 +1,336 @@
+<section class="bg-gradient text-light p-0">
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-12">
+                <div class="space-lg pb-0">
+                    <h1 class="display-4 no-wrap">Французская Полинезия</h1>
+                    <div class="lead">
+                        <b>Столица</b>: Папеэте (о. Таити)<br>
+                        <b>Официальный язык</b>: французский<br>
+                        <b>Валюта</b>: французский тихоокеанский франк (XPF)<br>
+                        <b>Территория</b>: 4 167 км²<br>
+                        <b>Население</b>: 280.2 тыс человек<br>
+                        <b>Часовой пояс</b>: UTC -10. Разница с Астаной -16 часов.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <svg class="decorative-divider" preserveAspectRatio="none" viewBox="0 0 100 100">
+        <polygon fill="#F8F9FB" points="0 100 100 100 100 0"></polygon>
+    </svg>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <p>Граничит на западе с водами Островов Кука, на северо-западе — с Республикой Кирибати, на севере, востоке и юге — с нейтральными тихоокеанскими водами, на юго-востоке — с водами Питкэрна.</p>
+
+                <!--Карта-->
+                <?= $this->render('_map', $country) ?>
+
+                <p>Климат на большей части Французской Полинезии, преимущественно в центре, тропический, пассатный. На севере (Маркизские острова) — субэкваториальный. В южной части наблюдаются заметные различия между тёплой зимой и жарким летом. На южных островах Тубуаи климат субтропический влажный. Наиболее жаркий и влажный сезон — с ноября по май, когда часто дуют северо-западные ветры. В самые жаркие месяцы температура обычно 27 — 32 °C и влажность воздуха очень высока (около 92 %) а осадков выпадает до 1700 мм (75 % годовой нормы). Осадки в это время выпадают, преимущественно, в виде мощных, но кратковременных вечерних ливней, или затяжных моросящих дождей длительностью до нескольких суток.</p>
+                <p>В этот же период нередки тропические ураганы и мощные шторма. Острова Таити, Туамоту, Тубуаи подвержены им.</p>
+                <p>В сухой сезон (с июня по октябрь) преобладают юго-восточные пассаты. Ниже всего температуры опускаются в июле-августе (на юге — до 18 — 21 °C).</p>
+
+                <figure class="figure d-block">
+                    <div class="video-cover box-shadow">
+                        <img alt="Остров Таити, Французская Полинезия" src="https://i.ytimg.com/vi/CQx6x1vbWSU/maxresdefault.jpg" class="bg-image"/>
+                        <div class="video-play-icon">
+                            <i class="icon-controller-play"></i>
+                        </div>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" data-src="https://www.youtube.com/embed/CQx6x1vbWSU?autoplay=1&rel=0&showinfo=0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <figcaption class="figure-caption text-right">Остров Таити, Французская Полинезия<br>© Самый Настоящий 2012</figcaption>
+                </figure>
+
+                <hr>
+
+                <figure class="figure d-block">
+                    <div class="video-cover box-shadow">
+                        <img alt="Туристические жемчужины - Бора Бора / Bora Bora" src="https://i.ytimg.com/vi/-Okqwj9CvOE/maxresdefault.jpg" class="bg-image"/>
+                        <div class="video-play-icon">
+                            <i class="icon-controller-play"></i>
+                        </div>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" data-src="https://www.youtube.com/embed/-Okqwj9CvOE?autoplay=1&rel=0&showinfo=0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <figcaption class="figure-caption text-right">Туристические жемчужины - Бора Бора / Bora Bora<br>© Самый Настоящий 2013</figcaption>
+                </figure>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<nav id="scrollspy-navbar" class="bg-white navbar sticky-top">
+    <div class="container">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item"><a class="nav-link" href="#culture">Культура и традиции</a></li>
+            <li class="nav-item"><a class="nav-link" href="#visa">Въезд в страну</a></li>
+            <li class="nav-item"><a class="nav-link" href="#custom">Таможня</a></li>
+            <li class="nav-item"><a class="nav-link" href="#transport">Транспорт</a></li>
+            <li class="nav-item"><a class="nav-link" href="#auto">Авто</a></li>
+            <li class="nav-item"><a class="nav-link" href="#health">Здравоохранение</a></li>
+            <li class="nav-item"><a class="nav-link" href="#communication">Связь</a></li>
+            <li class="nav-item"><a class="nav-link" href="#sight">Что посмотреть</a></li>
+        </ul>
+    </div>
+</nav>
+
+<section id="culture">
+    <div class="container">
+        <h2>Культура и традиции</h2>
+
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-8 col-lg-7">
+                <p>Полинезийцы по своей природе очень жизнерадостные и дружелюбные люди, туристов холят и лелеют. Здесь редко увидишь нищего или попрошайку, нет проституции, никто не бегает за туристом с предложением купить чего-либо или обязательно посетить именно это место. Уровень сервиса высочайший.</p>
+                <p>Никаких особых ограничений в плане одежды на островах нет. Правда, разгуливать по Папеэте в купальных костюмах или шортах все же не следует.</p>
+
+                <p>Французская Полинезия - очень дорогая страна. Многие товары и услуги стоят в 2 раза выше, чем, например, в Австралии или в 3 раза выше, чем на близлежащих Островах Кука. Характерной чертой, определяющей уровень цен в том или ином заведении, является обозначение его категории значками доллара - дорогой ресторан обозначают надписями "$$$$", а недорогой бюджетный - "$". На Таити можно остановиться в отелях Sheraton или Meridien. Цены в этих отелях, соответствующих европейским пятизвездочным гостиницам, примерно одинаковы: сутки в бунгало на воде - 50-70 тыс. полинезийских франков. Бунгало на берегу и в саду дешевле.</p>
+
+                <p>Чаевые не приняты, но и не запрещаются.</p>
+                <p>Торговаться здесь не принято, это оскорбляет честность продавца, особенно если он – житель Полинезии. Но если Вы приобретаете дорогие драгоценности, можно просить о скидке.</p>
+
+                <h5>Безопасность</h5>
+                <p>Острова Французской Полинезии практически безопасны для путешественников. Преступления имущественного характера изредка фиксируются, но очень быстро раскрываются, что и понятно — на небольших островах все на виду и скрыться от закона затруднительно. А с учетом известной прямолинейности местных жителей — и попросту невозможно. Полиция жестко преследует любые виды преступлений, совершенных против иностранных граждан. Однако следует всегда предпринимать меры безопасности против карманников, особенно в районах большого скопления народа, следить за своими вещами и предпринимать обычные меры предосторожности.</p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="float-right icon-credit-card"></i>
+                        <span class="h6"><b>Банкоматы</b></span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p>Доллары США и EUR принимаются к оплате практически повсеместно, однако курс везде разный. Валюту можно обменять в аэропорту, в банке или в отелях. Выгоднее всего иметь с собой EUR, которые во всех отелях и обменных пунктах меняют по официальному курсу. Доллары часто принимают по не самому выгодному курсу. «Кредитками» можно воспользоваться в отелях, ресторанах, магазинах и других туристических заведениях.</p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <i class="float-right icon-wallet"></i>
+                        <span class="h6"><b>Уровень цен</b></span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p>Завтрак в отелях (он, как правило, не входит в стоимость номера) - примерно $20-25. Обед в ресторане на двоих - около $70.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-12 mt-4">
+                <h5>Праздничные и нерабочие дни</h5>
+
+                <div class="card bg-secondary">
+                    <div class="card-body">
+                        <ul>
+                            <li>1 января — Новый год</li>
+                            <li>29 марта — Великая пятница</li>
+                            <li>1 апреля — Поливальный понедельник</li>
+                            <li>1 мая — Праздник весны и труда</li>
+                            <li>8 мая — День Победы</li>
+                            <li>19 мая, 29 июня, 14 июля — День Святой Троицы (Whit Monday)</li>
+                            <li>15 августа — Вознесение Девы Марии</li>
+                            <li>1 ноября — День всех святых</li>
+                            <li>11 ноября — День ветеранов</li>
+                            <li>25 декабря — Рождество Христово</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
+
+<section id="visa" class="bg-white">
+    <div class="container">
+        <h2>Посещение гражданами Казахстана</h2>
+
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-8 col-lg-7">
+                <p>Необходимые документы для оформления визы:</p>
+                <ol>
+                    <li>Действующий загранпаспорт;</li>
+                    <li>Анкета-заявление;</li>
+                    <li>2 цветные фотографии заявителя 3,5*4,5;</li>
+                    <li>Страховка на весь срок пребывания;</li>
+                    <li>Копия удостоверения личности;</li>
+                    <li>Справка с места работы;</li>
+                    <li>Адресная справка;</li>
+                    <li>Внутрисемейные документы;</li>
+                    <li>Справка из банка о финансовой состоятельности;</li>
+                    <li>Бронь авиабилета туда и обратно;</li>
+                    <li>Бронь в отеле;</li>
+                    <li>Визовый сбор (60 Евро).</li>
+                </ol>
+                <p class="small">Информация предоставлена в ознакомительных целях и не является руководством к получению визы.</p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="float-right icon-v-card"></i>
+                        <span class="h6"><b>Виза:</b></span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Виза во Французскую Полинезию гражданам Казахстана необходима, выдается Французским посольством в Астане и консульством в Алматы. Она называется «виза для поездки на заморские территории Франции» и не является шенгенской. Срок оформления визы от 10 дней до 3-х недель.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="custom">
+    <div class="container">
+        <h2>Таможенный контроль</h2>
+
+        <div class="row justify-content-between">
+            <div class="col">
+                <p>Ввоз и вывоз местной и иностранной валют не ограничен, декларация необходима на сумму свыше 7500 EUR.</p>
+                <p>Беспошлинно можно ввозить до 200 сигарет или 100 сигарилл или 50 г табака, до 2 л вина и литра алкоголя крепче 22 °, до 500 г кофе, до 100 г чая, до 50 мл духов и 250 мл туалетной воды, а также других товаров на сумму до 5000 XPF. Запрещен ввоз всех продуктов питания животного происхождения, растений, плодов, оружия, боеприпасов и наркотиков, а также контрафактной продукции и искусственного жемчуга.</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section id="transport" class="bg-white">
+    <div class="container">
+        <h2>Транспорт</h2>      
+
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-8 col-lg-7">
+                <p>Основной транспорт Французской Полинезии воздушный. Практически все крупные острова имеют свои взлетные полосы, а местные авиакомпании обеспечивают регулярное сообщение.</p>
+                <p>Прямые рейсы не летают. Наиболее короткий вариант перелета на Таити – через Токио. Формально в Токио транзит безвизовый до 3-х суток, однако, если Вы планируете остановиться в Токио более чем на 1 ночь – рекомендуется оформить транзитную визу.</p>
+                <p>Также здесь развито судоходство. Однако и прокат автомобилей, как в любом цивилизованном курортном месте, возможен.</p>
+
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="float-right icon-aircraft"></i>
+                        <span class="h6"><b>Авиарейсы</b></span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            Прелет Токио-Таити выполняется 3 раза в неделю.
+                        </li>
+                    </ul>
+                    <div class="card-footer p-3">
+                        <a href="https://booking.scat.kz/websky/#/search" class="btn btn-block btn-outline-success">Подобрать рейс</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="auto">
+    <div class="container">
+        <h2>Автомобиль</h2>
+
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-8 col-lg-7">
+                <p>Водить во Французской Полинезии несложно: правила дорожного движения не отличаются от общеевропейских.</p><p>Дороги качественные, асфальтированные, с четкой разметкой. Ухабов и лежачих полицейских по минимуму.</p>
+                <p>Однако лучше не развивать слишком высокую скорость во время отдыха во Французской Полинезии: за городом не стоит ехать свыше 80 километров в час, в городе — свыше 60 километров в час.</p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <figure class="figure d-block">
+                    <div class="video-cover box-shadow">
+                        <img alt="Французская полинезия, Таити - автомобили и дороги" src="https://i.ytimg.com/vi/UcqGoqF-8RM/maxresdefault.jpg" class="bg-image"/>
+                        <div class="video-play-icon">
+                            <i class="icon-controller-play"></i>
+                        </div>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" data-src="https://www.youtube.com/embed/UcqGoqF-8RM?autoplay=1&rel=0&showinfo=0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <figcaption class="figure-caption text-right">Французская полинезия, Таити - автомобили и дороги<br>© inturistTV 2011</figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="health" class="bg-white">
+    <div class="container">
+        <h2>Здравоохранение</h2>
+
+        <div class="row justify-content-between">
+            <div class="col">
+                <p>Для въезда в страну не нужно делать никаких прививок. Здесь очень развитая современная структура здравоохранения, везде строго следят за гигиеной. Вода из-под крана в отелях и ресторанах пригодна для питья.</p>
+                <p>На острова стоит прихватить обувь с надежной подошвой: на многих рифовые участках хватает острых камешков, а в прибрежной зоне обитают колючие морские животные. В воду лучше входить в пластиковых тапочках — во избежание порезов и ожогов о кораллы. Ядовитых животных и насекомых на островах нет.</p>
+                <a href="http://medical.account.travel" class="btn btn-outline-success">Оформить туристическую страховку</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="communication">
+    <div class="container">
+        <h2>Связь и коммуникации</h2>
+
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-8 col-lg-7">
+                <p>Международные телефонные соединения предоставляются наземной спутниковой станцией Intelsat (Тихий океан). Сотовая связь стандарта GSM 900 на островах хорошо развита. Главные острова (Таити, Муреа, Бора-Бора, Тахаа, Раиатеа, Хуахин и Рангироа) охвачены полностью, мелкие островки часто находятся вне зоны охвата, но даже там связь относительно устойчивая. Роуминг с местной сетью Tikiphone SA доступен абонентам крупнейших операторов. Местные СИМ-карты можно приобрести в офисах Tikiphone SA.</p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="float-right icon-old-phone"></i>
+                        <span class="h6"><b>Полезные телефоны</b></span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <div class="d-flex justify-content-between">
+                                <div>Полиция</div>
+                                <span>17</span>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="d-flex justify-content-between">
+                                <div>Служба спасения</div>
+                                <span>15 или 423-456</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <h5>Телефоны</h5>
+
+                <div class="card bg-secondary">
+                    <div class="card-body">
+                        <ul>
+                            <li>Туристический офис Таити: 505-700</li>
+                            <li>Туристический офис Бора-Бора: 677-636, 677-031, 677-236</li>
+                            <li>Туристический офис Муреа: 562-909</li>
+                            <li>Туристический офис острова Хуахин: 688-634</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section id="sight" class="bg-white">
+    <div class="container">
+        <!--Блок экскурсий-->
+        <?= $this->render('_excursions', $country) ?>
+    </div>
+</section>
